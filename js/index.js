bc192841c2ba585644fd4915daa7272178c284c0
Milosz <57958312+rbnsnn@users.noreply.github.com>
2022-06-23 18:26:06 +0200

    add all content

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 const expandAbout = document.querySelector('.about__expand')
 const expandContent = document.querySelector('.about__expanded-content')
+const expandSpan = document.querySelector('.expand')
+const expandIcon = document.querySelector('.fa-solid')
 
-expandAbout.addEventListener('toggle', () => {
-    expandContent.classList.toggle()
+const menuToggle = document.querySelector('#menu__toggle')
+const menuItems = document.querySelectorAll('.menu__item')
+
+expandAbout.addEventListener('click', () => {
+    expandContent.classList.toggle('about__expanded-content--visible')
+    if (expandSpan.innerHTML === 'ROZWIŃ') {
+        expandSpan.innerHTML = 'ZWIŃ'
+        expandIcon.classList = 'fa-solid fa-angles-up'
+    } else {
+        expandSpan.innerHTML = "ROZWIŃ"
+        expandIcon.classList = 'fa-solid fa-angles-down'
+    }
+})
+
+menuItems.forEach(element => {
+    element.addEventListener('click', () => {
+        menuToggle.checked = false
+    })
 })
